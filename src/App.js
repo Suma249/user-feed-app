de4fbdf7e733dd49d22cdbb28b1d856bc8707e62
Suma249 <3584_suma@katalystindia.org>
2024-09-02 18:49:23 +0530

    added header and structured the output

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
 import FectchData from './Components/FectchData';
+import Header from './Components/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <FectchData/>
     </div>
   );
--- src/Components/FectchData.js
+++ src/Components/FectchData.js
@@ -8,9 +8,12 @@ function FectchData() {
     const updateData=(data)=>{
         setData(data);
     }
+    const style={
+      "list-style-type":"none"
+    }
     useEffect(
         ()=>{
-         axios.get("https://randomuser.me/api?results=2")
+         axios.get("https://randomuser.me/api?results=1")
          .then( response =>{
             console.log("data fetched is: "+response.data.results);
             updateData(response.data.results);
@@ -25,7 +28,27 @@ function FectchData() {
   return (
     <div>
      {
-        data.map( (d,index) =><li key={index}><div>{d.name.first}</div><div>{d.name.last}</div><div>{d.email}</div></li>)
+        data.map(
+          (d,index) =>
+          
+          <li key={index} style={style}>
+            <div class="userDetails">
+            <div class="userHeader">
+               <h2>User: {d.login.username}</h2>
+            </div>
+            <div class="userBody">
+             <h2>{d.name.title} {d.name.first} {d.name.last}</h2>
+             <p>{d.email}</p>
+             <br/>
+             <h3>User for {d.registered.age} years</h3>
+             <p>Age: {d.dob.age}</p>
+             <p>Nationality: {d.nat}</p>
+             <p>Phone: {d.phone}</p>
+            </div>
+            </div>
+         </li>
+          
+         )
      }
     </div>
   )
